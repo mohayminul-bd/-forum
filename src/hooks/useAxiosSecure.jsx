4b27b0b71d4e73454331b6448ac3810521dc26bf
mohayminul-bd <mohayminulislam514@gmail.com>
2025-09-30 22:17:53 +0600

    deploy problem solve

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 import useAuth from "./useAuth";
 const axiosSecure = axios.create({
-  baseURL: `http://localhost:5000`,
+  baseURL: `http://localhost:3000`,
 });
 const useAxiosSecure = () => {
   const { user } = useAuth();
